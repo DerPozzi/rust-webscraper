42132555a608d4bae86983de98b855f7548e84bf
Gianluca Pozzo <gianluca.pozzo@sva.de>
2023-10-06 13:54:52 +0200

    Successfull get title and date

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest;
-use std::fs;
+use select::{self, document::Document, predicate::Class};
+use std::{fs, thread::JoinHandle};
 
 #[tokio::main]
 async fn main() {
@@ -14,4 +15,40 @@ async fn main() {
         }
         Err(msg) => panic!("{}", msg),
     }
+
+    let mut joins: Vec<tokio::task::JoinHandle<()>> = Vec::new();
+
+    for url in url_vec {
+        joins.push(tokio::spawn(async move {
+            match get_date_and_title(url).await {
+                Err(e) => println!("ERROR: {}", e),
+                _ => {}
+            }
+        }))
+    }
+
+    for task in joins {
+        task.await.expect("ERROR");
+    }
+}
+
+async fn get_date_and_title(url: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello from Thread");
+
+    let html = reqwest::get(url).await?.text().await?;
+
+    let document = Document::from(html.as_str());
+    let title = document
+        .find(Class("entry-title"))
+        .nth(0)
+        .expect("No Title given")
+        .text();
+    let date = document
+        .find(Class("meta-date"))
+        .nth(0)
+        .expect("No date given")
+        .text();
+
+    println!("The article \"{}\" was published on [{}]", title, date);
+    Ok(())
 }
