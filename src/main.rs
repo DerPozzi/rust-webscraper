e7a19f838e1f5da26c8c2dacad4c15eb827f6447
Gianluca Pozzo <gianluca.pozzo@sva.de>
2023-10-06 09:25:31 +0200

    Get urls from txt file provided by argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    let file = std::env::args().nth(1).expect("Expected one argument");
+    let mut url_vec: Vec<String> = Vec::new();
+    match fs::read_to_string(file) {
+        Ok(all_urls) => {
+            let lines = all_urls.lines();
+            for line in lines {
+                url_vec.push(line.to_string());
+            }
+        }
+        Err(msg) => panic!("{}", msg),
+    }
 }
